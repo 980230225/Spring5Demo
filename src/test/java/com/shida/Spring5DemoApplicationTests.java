387af7a066f51ecc265414717412bd82ccc71ba4
980230225 <a980230225@qq.com>
2021-08-16 16:30:36 +0800

    Initial commit

---
+++ src/test/java/com/shida/Spring5DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
